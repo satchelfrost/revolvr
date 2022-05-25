bfb0171476809d46629af255067f21f4b7aed8bd
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-05-25 09:55:44 -0400

    I like it how you added the relative includes for the cpp's inside of src. This technically means that we don't have to include this in the LOCAL_C_INCLUDES. This is also how the core guidelines recommends. See SF.12. Let's stick with this convention.

--- FTS/Android.mk
+++ FTS/Android.mk
@@ -5,14 +5,11 @@ LOCAL_MODULE := fts
 
 include ../cflags.mk
 
-LOCAL_C_INCLUDES := \
-  					$(LOCAL_PATH)/../libs/khronos/openxr/OpenXR-SDK/include \
-  					$(LOCAL_PATH)/src/main/cpp/include \
-
-LOCAL_SRC_FILES	:= $(wildcard $(LOCAL_PATH)/src/main/cpp/*.cpp)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../libs/khronos/openxr/OpenXR-SDK/include
+LOCAL_SRC_FILES  := $(wildcard $(LOCAL_PATH)/src/main/cpp/*.cpp)
 
 # include default libraries
-LOCAL_LDLIBS 			:= -llog -landroid -lvulkan
+LOCAL_LDLIBS           := -llog -landroid -lvulkan
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 LOCAL_SHARED_LIBRARIES := openxr_loader
 
