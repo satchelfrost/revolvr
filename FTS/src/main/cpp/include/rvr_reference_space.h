002462b5be051bf6942f160994049918f9e35a6b
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-06-25 21:04:50 -0400

    Added a toString for the reference space enum

--- FTS/src/main/cpp/include/rvr_reference_space.h
+++ FTS/src/main/cpp/include/rvr_reference_space.h
@@ -10,3 +10,27 @@ enum class RVRReferenceSpace {
     RVRStageRightRotated = 6,
     RVRStageLeftRotated = 7
 };
+
+inline const char* toString(RVRReferenceSpace space) {
+    switch (space) {
+    case RVRReferenceSpace::RVRHead:
+        return "RVRHead";
+    case RVRReferenceSpace::RVRHud:
+        return "RVRHud";
+    case RVRReferenceSpace::RVRHeadInitial:
+        return "RVRHeadInitial";
+    case RVRReferenceSpace::RVRTrackedOrigin:
+        return "RVRTrackedOrigin";
+    case RVRReferenceSpace::RVRStageRight:
+        return "RVRStageRight";
+    case RVRReferenceSpace::RVRStageLeft:
+        return "RVRStageLeft";
+    case RVRReferenceSpace::RVRStageRightRotated:
+        return "RVRStateRightRotated";
+    case RVRReferenceSpace::RVRStageLeftRotated:
+        return "RVRStageLeftRotated";
+    default:
+        return "Unknown RVRReferenceSpace type";
+    }
+
+}
