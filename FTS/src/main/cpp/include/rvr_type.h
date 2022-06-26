6c849391fe49aec5c13d40c66627b85db9b25047
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-06-25 21:05:23 -0400

    Added a toString for the rvr type enum

--- FTS/src/main/cpp/include/rvr_type.h
+++ FTS/src/main/cpp/include/rvr_type.h
@@ -5,3 +5,16 @@ enum class RVRType {
     RVRSpatial = 1,
     RVRMesh = 2
 };
+
+inline const char* toString(RVRType rvrType) {
+    switch (rvrType) {
+    case RVRType::RVRObject:
+        return "RVRObject";
+    case RVRType::RVRSpatial:
+        return "RVRSpatial";
+    case RVRType::RVRMesh:
+        return "RVRMesh";
+    default:
+        return "RVRType unrecognized";
+    }
+}
