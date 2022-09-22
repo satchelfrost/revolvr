9c94f521894b402f2f1f38e503de79c53590b1d0
Reese Gallagher <satchelfrost@gmail.com>
2022-09-21 22:41:41 -0400

    Added tree functionality to spatial

--- FTS/src/main/cpp/engine/ecs/component/types/spatial.cpp
+++ FTS/src/main/cpp/engine/ecs/component/types/spatial.cpp
@@ -1,10 +1,38 @@
-#include "include/ecs/component/types/Spatial.h"
+#include "include/ecs/component/types/spatial.h"
 
 namespace rvr {
 Spatial::Spatial() : Component(ComponentType::Spatial),
-                               pose(RVRMath::Pose::Identity()),
-                               worldPose(RVRMath::Pose::Identity()),
-                               scale(RVRMath::Vector::One()) {}
+                     pose(RVRMath::Pose::Identity()),
+                     worldPose(RVRMath::Pose::Identity()),
+                     scale(RVRMath::Vector::One()),
+                     parent_(nullptr) {}
+
+void Spatial::AddChild(Spatial* child) {
+    child->SetParent(this);
+    children_.push_back(child);
+}
+
+void Spatial::RemoveFromParent() {
+    if (parent_) {
+        auto& children = parent_->GetChildren();
+        auto childItr = std::find(children.begin(), children.end(), this);
+        CHECK(childItr != children.end());
+        children.erase(childItr);
+    }
+}
+
+void Spatial::SetParent(Spatial* parent) {
+    RemoveFromParent();
+    parent_ = parent;
+}
+
+const Spatial* Spatial::GetParent() {
+    return parent_;
+}
+
+std::list<Spatial*>& Spatial::GetChildren() {
+    return children_;
+}
 
 void Spatial::UniformScale(float scaleFactor) {
     scale.x = scale.x * scaleFactor;
--- FTS/src/main/cpp/engine/include/ecs/component/types/spatial.h
+++ FTS/src/main/cpp/engine/include/ecs/component/types/spatial.h
@@ -3,14 +3,25 @@
 #include "ecs/ecs.h"
 #include "ecs/component/component.h"
 #include "math/rvr_math.h"
+#include "check.h"
 
 namespace rvr {
 class Spatial : public Component {
 public:
     Spatial();
+    void AddChild(Spatial* child);
+    void SetParent(Spatial* parent);
+    const Spatial* GetParent();
+    std::list<Spatial*>& GetChildren();
     void UniformScale(float scaleFactor);
+
     XrPosef pose;
     XrPosef worldPose;
     XrVector3f scale;
+
+private:
+    void RemoveFromParent();
+    Spatial* parent_;
+    std::list<Spatial*> children_;
 };
 }
