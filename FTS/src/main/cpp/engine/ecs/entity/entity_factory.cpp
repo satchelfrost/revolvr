281e9df32ebf1dcd1537279a5cc405bb1c7f2251
Reese Gallagher <satchelfrost@gmail.com>
2022-09-21 22:45:21 -0400

    If check uses assign boolean to print warning

--- FTS/src/main/cpp/engine/ecs/entity/entity_factory.cpp
+++ FTS/src/main/cpp/engine/ecs/entity/entity_factory.cpp
@@ -7,7 +7,9 @@ entityPool_(entityPool), componentPoolManager_(componentPoolManager) {}
 Entity *EntityFactory::CreateEntity(const std::vector<ComponentType>& cTypes) {
     Entity* entity = entityPool_->GetNextEntity(cTypes);
     for (auto cType : cTypes)
-        componentPoolManager_->Assign(entity->id, cType);
+        if (!componentPoolManager_->Assign(entity->id, cType))
+            Log::Write(Log::Level::Warning,
+                       Fmt("Entity %d could not be assigned component %s", entity->id, toString(cType)));
     return entity;
 }
 }
