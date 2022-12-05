ae8c3d12ed7de91ad920116588a7d96c3156359b
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-12-04 21:02:40 -0500

    Deleted dead code. Build still working.

--- FTS/src/main/cpp/engine/objects/rvr_hand.cpp
+++
@@ -1,3 +0,0 @@
-#include "objects/rvr_hand.h"
-
-RVRHand::RVRHand(int id, int side) : RVRSpatial(id, RVRType::Hand), side_(side) {}
--- FTS/src/main/cpp/engine/objects/rvr_origin.cpp
+++
@@ -1,3 +0,0 @@
-#include "objects/rvr_origin.h"
-
-RVROrigin::RVROrigin(int id) : RVRSpatial(id, RVRType::Origin) {}
