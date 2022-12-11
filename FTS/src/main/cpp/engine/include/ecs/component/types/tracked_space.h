0441658e5a8b35b2e6bbf47326e3be87afc8c043
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-12-11 18:07:31 -0500

    Tracked space type has x macro, toString, and toEnum.

--- FTS/src/main/cpp/engine/include/ecs/component/types/tracked_space.h
+++ FTS/src/main/cpp/engine/include/ecs/component/types/tracked_space.h
@@ -2,17 +2,31 @@
 
 #include "ecs/component/component.h"
 
+#define LAST_TRACKED_SPACE 2
+#define TRACKED_SPACE_LIST(X) \
+    X(VROrigin, 0) \
+    X(LeftHand, 1) \
+    X(RightHand, LAST_TRACKED_SPACE)           \
+
+
+#define BUILD_ENUM(ENUM, NUM) ENUM = NUM,
+
 namespace rvr {
+namespace constants {
+    const int NUM_TRACKED_SPACES = LAST_TRACKED_SPACE + 1;
+}
+
 enum class TrackedSpaceType {
-    VROrigin,
-    LeftHand,
-    RightHand,
+    TRACKED_SPACE_LIST(BUILD_ENUM)
     Nil
 };
 
 class TrackedSpace : public Component {
 public:
     TrackedSpace();
-    TrackedSpaceType trackedSpaceType;
+    TrackedSpaceType type;
 };
+
+const char *toString(TrackedSpaceType cType);
+TrackedSpaceType toTrackedSpaceTypeEnum(const std::string& str);
 }
