0e63403732f539e6dcfc1323ef44ccce0db23f1c
Cristhian De La Paz <cdelapaz@live.com>
2023-03-02 16:14:30 -0500

    Updated Spatial and Spatial System

--- FTS/src/main/cpp/engine/ecs/system/spatial_system.cpp
+++ FTS/src/main/cpp/engine/ecs/system/spatial_system.cpp
@@ -15,13 +15,16 @@ void UpdateTrackedSpaces(XrContext *context) {
         auto [spatial, tracked] = ECS::Instance()->GetComponentPair<Spatial, TrackedSpace>(entityId);
         switch (tracked->type) {
             case TrackedSpaceType::VROrigin:
-                spatial->world = math::Transform(trackedSpaceLocations.vrOrigin.pose, spatial->world.scale);
+                spatial->SetWorld(math::Transform(trackedSpaceLocations.vrOrigin.pose,
+                                                  spatial->GetWorld().GetScale()));
                 break;
             case TrackedSpaceType::LeftHand:
-                spatial->world = math::Transform(trackedSpaceLocations.leftHand.pose, spatial->world.scale);
+                spatial->SetWorld(math::Transform(trackedSpaceLocations.leftHand.pose,
+                                                  spatial->GetWorld().GetScale()));
                 break;
             case TrackedSpaceType::RightHand:
-                spatial->world = math::Transform(trackedSpaceLocations.rightHand.pose, spatial->world.scale);
+                spatial->SetWorld(math::Transform(trackedSpaceLocations.rightHand.pose,
+                                                  spatial->GetWorld().GetScale()));
                 break;
             default:
                 break;
@@ -29,44 +32,9 @@ void UpdateTrackedSpaces(XrContext *context) {
     }
 }
 
-void UpdateWorldTransform(type::EntityId id) {
-    Entity* child = ECS::Instance()->GetEntity(id);
-    if (id == constants::ROOT_ID)
-        return;
-
-    auto parent = child->GetParent();
-    UpdateWorldTransform(parent->id);
-    auto childSpatial = ECS::Instance()->GetComponent<Spatial>(child->id);
-    auto parentSpatial = ECS::Instance()->GetComponent<Spatial>(parent->id);
-
-    if (child->HasComponent(ComponentType::TrackedSpace)) {
-        childSpatial->world.scale = childSpatial->local.scale * parentSpatial->world.scale;
-    }
-    else {
-        // Calculate position based on parent
-        childSpatial->world.pose.orientation = childSpatial->local.GetOrientation()
-                                               * parentSpatial->world.GetOrientation();
-
-        glm::vec3 offset = glm::rotate(parentSpatial->world.GetOrientation(),
-                                       childSpatial->local.GetPosition());
-
-        childSpatial->world.pose.position = offset + parentSpatial->world.GetPosition();
-        childSpatial->world.scale = childSpatial->local.scale * parentSpatial->world.scale;
-
-        // Place objects relative to the origin
-        if (parent->HasComponent(ComponentType::TrackedSpace)) {
-            auto ts = ECS::Instance()->GetComponent<TrackedSpace>(parent->id);
-            if (ts->type == TrackedSpaceType::VROrigin) {
-                childSpatial->world.pose.position = childSpatial->world.GetPosition()
-                                                    - parentSpatial->local.GetPosition();
-            }
-        }
-    }
-}
-
 void UpdateSpatials() {
     auto components = ECS::Instance()->GetComponents(ComponentType::Spatial);
     for (auto [eid, component] : components)
-        UpdateWorldTransform(component->id);
+        dynamic_cast<Spatial *>(component)->UpdateWorld();
 }
 }
--- FTS/src/main/cpp/engine/include/ecs/component/types/spatial.h
+++ FTS/src/main/cpp/engine/include/ecs/component/types/spatial.h
@@ -7,10 +7,25 @@ namespace rvr {
 class Spatial : public Component {
 public:
     Spatial(type::EntityId pId);
+    Spatial(type::EntityId pId, const glm::vec3& position, const glm::quat& orientation, const glm::vec3& scale);
 
-    // TODO: Make local and world transforms private
-    // TODO: Move the UpdateWorld function out of SpatialSystem
-    // TODO: Add convenience functions to Spatial
+    math::Transform GetLocal();
+    void SetLocal(const math::Transform& value);
+    void SetLocalPose(const math::Pose& pose);
+    void SetLocalScale(const glm::vec3& scale);
+    void SetLocalPosition(const glm::vec3& position);
+    void SetLocalOrientation(const glm::quat& orientation);
+
+    math::Transform GetWorld();
+    void SetWorld(const math::Transform& value);
+    void SetWorldPose(const math::Pose& pose);
+    void SetWorldScale(const glm::vec3& scale);
+    void SetWorldPosition(const glm::vec3& position);
+    void SetWorldOrientation(const glm::quat& orientation);
+
+    void UpdateWorld();
+
+private:
     math::Transform local;
     math::Transform world;
 };
