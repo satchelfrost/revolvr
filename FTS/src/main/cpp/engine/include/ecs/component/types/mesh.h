341570e394a8f4ad2882b106af50e38854a52e8f
Reese Gallagher <reese.gallagher@infinitasinc.com>
2023-05-10 13:00:05 -0400

    Add recursive visibility setter which sets the visibility of the mesh as well as its children

--- FTS/src/main/cpp/engine/include/ecs/component/types/mesh.h
+++ FTS/src/main/cpp/engine/include/ecs/component/types/mesh.h
@@ -7,6 +7,7 @@ class Mesh : public Component {
 public:
     Mesh(type::EntityId pId);
     virtual Component* Clone(type::EntityId newEntityId) override;
+    void SetVisibilityRecursive(bool visibility);
     bool visible;
 private:
     //Resource resource_
