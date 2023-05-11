0fb01a19ecc05a6062e5b6297bfa84acc081db5d
Reese Gallagher <reese.gallagher@infinitasinc.com>
2023-05-10 22:00:10 -0400

    Moved audio mixer class into audio system to make it more clear that it is the thing acting on the audio components

--- FTS/src/main/cpp/engine/include/ecs/system/audio_system.h
+++ FTS/src/main/cpp/engine/include/ecs/system/audio_system.h
@@ -7,4 +7,11 @@ aaudio_data_callback_result_t dataCallback(AAudioStream *stream, void *userData,
                                            int32_t numFrames);
 
 void errorCallback(AAudioStream *stream, void *userData, aaudio_result_t error);
+
+class Mixer {
+public:
+    void Render(float* audioData, int32_t numFrames);
+private:
+    float mixingBuffer_[1920];
+};
 }
