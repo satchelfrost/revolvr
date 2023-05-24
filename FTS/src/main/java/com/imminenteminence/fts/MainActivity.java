2cd41ce7e5e53d72f2067165e4afb32223400283
Reese Gallagher <reese.gallagher@infinitasinc.com>
2023-05-24 00:01:54 -0400

    Got rid of MainActivity

--- FTS/src/main/java/com/imminenteminence/fts/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.imminenteminence.fts;
-
-// BOOYAAHH
-public class MainActivity extends android.app.NativeActivity {
-  static {
-    System.loadLibrary("openxr_loader");
-    System.loadLibrary("fts");
-  }
-}
