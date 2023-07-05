1646f55c3784b2080018900c37b1e585acb13ecb
Cristhian De La Paz <cris.delapaz@infinitasinc.com>
2023-07-05 14:45:55 -0400

    Fixed a regression in common.h

--- include/common.h
+++ include/common.h
@@ -44,44 +44,6 @@ MAKE_TO_STRING_FUNC(XrSessionState);
 MAKE_TO_STRING_FUNC(XrResult);
 MAKE_TO_STRING_FUNC(XrFormFactor);
 
-inline bool EqualsIgnoreCase(const std::string& s1, const std::string& s2, const std::locale& loc = std::locale()) {
-    const std::ctype<char>& ctype = std::use_facet<std::ctype<char>>(loc);
-    const auto compareCharLower = [&](char c1, char c2) { return ctype.tolower(c1) == ctype.tolower(c2); };
-    return s1.size() == s2.size() && std::equal(s1.begin(), s1.end(), s2.begin(), compareCharLower);
-}
-
-struct IgnoreCaseStringLess {
-    bool operator()(const std::string& a, const std::string& b, const std::locale& loc = std::locale()) const noexcept {
-        const std::ctype<char>& ctype = std::use_facet<std::ctype<char>>(loc);
-        const auto ignoreCaseCharLess = [&](char c1, char c2) { return ctype.tolower(c1) < ctype.tolower(c2); };
-        return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end(), ignoreCaseCharLess);
-    }
-};
-
-template <typename T>
-struct ScopeGuard {
-    // Needs C++17: static_assert(std::is_invocable_v<T>, "Type must be invocable function.");
-
-    ScopeGuard(T&& guard) noexcept : m_guard(std::move(guard)) {}
-
-    ScopeGuard(ScopeGuard&&) noexcept = default;
-    ScopeGuard& operator=(ScopeGuard&&) noexcept = default;
-
-    ScopeGuard(ScopeGuard&) = delete;
-    ScopeGuard& operator=(ScopeGuard&) = delete;
-
-    ~ScopeGuard() { m_guard(); }
-
-   private:
-    T m_guard;
-};
-
-// Usage: auto guard = MakeScopeGuard([&] { foobar; });
-template <typename T>
-ScopeGuard<T> MakeScopeGuard(T&& guard) {
-    return ScopeGuard<T>(std::forward<T>(guard));
-}
-
 inline std::string Fmt(const char* fmt, ...) {
     va_list vl;
     va_start(vl, fmt);
